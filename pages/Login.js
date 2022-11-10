15b16cd826a2dc8777ad71c98feb0a5238333c28
Chris Tomshack <ctomshack15@gmail.com>
2022-11-10 14:11:06 -0500

    temp. removed google signIn option from login form

--- pages/Login.js
+++ pages/Login.js
@@ -99,7 +99,7 @@ export default function Login() {
               </div>
             </form>
 
-            <div className="mt-6">
+            {/* <div className="mt-6">
               <div className="relative">
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-300" />
@@ -123,7 +123,7 @@ export default function Login() {
 
                 
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
