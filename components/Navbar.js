df179670b7317b9f58df0d5031360fee0a19ec0d
Chris Tomshack <ctomshack15@gmail.com>
2022-11-10 14:52:13 -0500

    replaced bell icon with Logout button (added href)

--- components/Navbar.js
+++ components/Navbar.js
@@ -65,6 +65,7 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex flex-nowrap items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <Link href='/'>
                 <button
                   type="button"
                   className="inline-block rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -73,6 +74,7 @@ export default function Navbar() {
                   {/* <span className="sr-only">Logout</span> */}
                   {/* <ArrowLeftOnRectangleIcon className="h-5 w-5 inline-block ml-2" aria-hidden="true" /> */}
                 </button>
+                </Link>
 
                 
               </div>
